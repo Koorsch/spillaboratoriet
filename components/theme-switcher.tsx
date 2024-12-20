97da845b75d15b92bf3e1d141d5f43b32544f094
AndreasEkvall <anek0001@stud.kea.dk>
2024-12-20 09:09:04 +0100

    chore: last push?

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -11,7 +11,7 @@ import {
 import { Laptop, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-
+// this is the devil.
 const ThemeSwitcher = () => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
